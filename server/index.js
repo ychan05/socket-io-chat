aff871bd050a2a722ddefc706fe4059f6919561b
ychan05 <yatlongc2005@gmail.com>
2023-12-10 01:58:30 -0500

    start listening to connections

--- server/index.js
+++ server/index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors'); 
+const {Server} = require('socket.io');
 
 app.use(cors());
 
-const server = http.createServer(app);
 
-server.listen(54321, () => {
+const httpServer = http.createServer(app); // create a http server
+const io = new Server(httpServer, { // create a socket io server
+    cors: {
+        origin: 'http://localhost:54321', // where the react app is hosted
+        methods: ['GET', 'POST'] // allowed methods
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + ' disconnected');
+    }); 
+});
+
+httpServer.listen(54321, () => {
     console.log('server started!')
-})
+});
